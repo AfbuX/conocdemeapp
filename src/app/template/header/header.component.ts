254419b3c97485b90d0d8acf59e31285d31602f4
AfbuX <andres.betancur@correo.tdea.edu.co>
2025-04-09 20:41:55 -0500

    primera cargar

---
+++ src/app/template/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: false,
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+titulo: string = "Cualquier cosa"
+
+function goTolistar() {
+
+  
+}
+function ChangeValue(){
+  
+}
+}
